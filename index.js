4c2e0741bb635265e13bc17c59a6568a3c95c7fc
Varun Madhukara <mvarun@hotmail.co.uk>
2018-02-07 17:57:33 +0000

    OAuth 2

--- index.js
+++ index.js
@@ -1,24 +1,36 @@
 const express = require("express");
 
-const passport = require("passport");  // Gives express the idea of how to handle authentication
+const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
-// Instruct passport to exactly how to authenticate our users
-const keys = require('./config/keys')
+const keys = require("./config/keys");
 
 const app = express();
 
-// console.developers.google.com
-passport.use(new GoogleStrategy({
-  clientID: keys.googleClientID,
-  clientSecret: keys.googleClientSecret,
-  callbackURL: '/auth/google/callback'
-  }, (accessToken) => {
-     console.log(accessToken);
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback"
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log('access token', accessToken);
+      console.log('refresh token', refreshToken);
+      console.log('profile', profile);
+    }
+  )
+);
+
+// Adds route handler, 'get' type http request
+app.get(
+  "/auth/google", // first argument, kicks user into OAuth flow
+  passport.authenticate("google", {
+    scope: ["profile", "email"]   // asking google for profile and email
   })
 );
 
-// initializes GoogleStrategy
-// and tells Passport that there is a new strategy available and we want to use it
+app.get('/auth/google/callback', passport.authenticate('google'));
+// Exchanges the code received by google, to access the user profile
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
