0fa1594f384ce7cd84cdc66167cc57829c38015d
Varun Madhukara <mvarun@hotmail.co.uk>
2018-02-08 11:45:43 +0000

    Before Mongo model

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   googleClientID: '587696007461-1clbi99ucak0tnjpr92t6imgrsmit97l.apps.googleusercontent.com',
   googleClientSecret: 'ZAGedWWS1KE7OgZL6gr-N5yS'
+  mongoURI: 'mongodb://varun:password@ds127888.mlab.com:27888/userdb1'
 };
--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require("express");
-require('./services/passport');
+const mongoose = require("mongoose");
+const keys = require("./config/keys");
+require("./services/passport");
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
-require('./routes/authRoutes')(app); // returns a function and calls app object
+require("./routes/authRoutes")(app); // returns a function and calls app object
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
